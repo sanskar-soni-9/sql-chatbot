454e6e74ca5fc3295f4e4ab28b3f7d70e7e69870
Immortal <sanskarsoni300@gmail.com>
2024-03-10 16:03:24 +0530

    Added Auth, User & Config Modules in AppModule, .env.example file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import coreConfig from './config/core.config';
+import jwtConfig from './config/jwt.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [coreConfig, jwtConfig] }),
+    AuthModule,
+    UserModule,
+  ],
 })
 export class AppModule {}

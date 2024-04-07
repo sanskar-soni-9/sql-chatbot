f501faba488fe2e742a54edc421727d343518921
Immortal <sanskarsoni300@gmail.com>
2024-04-07 07:14:27 +0530

    backend - removed UsersModule from AppModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import coreConfig from './config/core.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { BotModule } from './modules/bot/bot.module';
-import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [coreConfig] }),
     AuthModule,
-    UsersModule,
     BotModule,
   ],
 })

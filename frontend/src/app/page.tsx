abf5d38fe4dc74097a03da2ae90d8d31263700db
Immortal <sanskarsoni300@gmail.com>
2024-04-10 20:08:06 +0530

    frontend - added homepage UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,18 @@
+import PrimaryButton from "@/components/PrimaryButton";
+
 export default function Home() {
   return (
-    <main className="">
-      <div className=""></div>
-    </main>
+    <div className="w-full h-full">
+      <main className="container mx-auto h-full flex flex-col items-center justify-center pb-20 gap-5">
+        <div className="flex flex-col text-4xl font-extrabold tracking-wide md:text-[56px]">
+          SQL ChatBot
+        </div>
+        <div className="flex flex-col items-center space-y-2 md:mt-6 md:flex-row md:space-x-4 md:space-y-0">
+          <PrimaryButton isLink href="/chat">
+            Generate SQL
+          </PrimaryButton>
+        </div>
+      </main>
+    </div>
   );
 }

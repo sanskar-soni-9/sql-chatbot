057dcb3d6751bf8064f1b895db272d5dcceadca3
Immortal <sanskarsoni300@gmail.com>
2024-04-11 07:07:15 +0530

    frontend - PrimaryButton added type and handleSubmit

--- frontend/src/components/PrimaryButton.tsx
+++ frontend/src/components/PrimaryButton.tsx
@@ -6,23 +6,32 @@ interface PrimaryButtonProps {
   isLink?: boolean;
   href?: string;
   handleClick?: () => void;
+  type?: "button" | "reset" | "submit";
+  handleSubmit?: () => void;
 }
 
 const BUTTON_STYLES =
-  "text-base flex h-[36px] min-w-[90px] cursor-pointer items-center justify-center rounded-lg border-2 border-action bg-action py-2 px-3 font-semibold text-white hover:opacity-50";
+  "text-base flex h-[36px] min-w-[90px] w-full cursor-pointer items-center justify-center rounded-lg border-2 border-action bg-action py-2 px-3 font-semibold text-white hover:opacity-50";
 
 const PrimaryButton: FC<PrimaryButtonProps> = ({
   children,
   isLink = false,
   href = "/",
   handleClick = () => {},
+  type,
+  handleSubmit = () => {},
 }) => {
   return isLink ? (
     <Link href={href} className={BUTTON_STYLES}>
       {children}
     </Link>
   ) : (
-    <button onClick={handleClick} className={BUTTON_STYLES}>
+    <button
+      onClick={handleClick}
+      onSubmit={handleSubmit}
+      className={BUTTON_STYLES}
+      type={type}
+    >
       {children}
     </button>
   );

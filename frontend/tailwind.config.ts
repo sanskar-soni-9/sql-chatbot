245bdaa0f70bc2faf60abd731affa9eb2b90effd
Immortal <sanskarsoni300@gmail.com>
2024-04-11 07:04:22 +0530

    frontend - tailwindConfig extend colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,12 +8,14 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      primary: colors.neutral[950],
-      secondary: colors.neutral[800],
-      action: colors.blue[500],
-      light: colors.neutral[50],
-      border: colors.neutral[800],
+    extend: {
+      colors: {
+        primary: colors.neutral[950],
+        secondary: colors.neutral[800],
+        action: colors.blue[500],
+        light: colors.neutral[50],
+        border: colors.neutral[800],
+      },
     },
   },
   plugins: [],
